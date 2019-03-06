6f40af6fa84c3aab1bb99884303bb5beb67428b9
Anam140601 <khoerulanam140601>
2019-03-06 10:57:55 +0700

    coba file dari mirukusheki

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, {Component} from "react";
+import logo               from "./img/yes.svg";
+import suara              from "./img/ashiap.mp3";
+import About              from './component/About';
+import Background         from './component/Background';
+import Foreground         from './component/Foreground';
+import Video              from './component/Video';
+import "./App.css";
+
+class App extends Component {
+  state = {
+    timesClicked: 0
+  };
+
+  playMusic = () => {
+    const audio = new Audio(suara);
+    audio.play();
+    this.setState({
+      timesClicked: this.state.timesClicked + 1
+    });
+  };
+
+  render() {
+    return (
+      <div className="App" onClick={e => this.playMusic()}>
+        <Foreground/>
+        <Video/>
+        <Background/>
+        <div className="content">
+          <header className="App-header">
+            <h1 style={{margin: '0'}}>ASHIYAAP.exe</h1>
+            <div>
+              <embed src={logo} className="App-logo" style={{height: '17em'}}/>
+            </div>
+            <div style={{marginTop: '1em'}}>
+              {this.state.timesClicked === 0 ?
+                <div>Klik biar Atta ngomong "Ashiap".</div> :
+                <div>Atta di klik: {this.state.timesClicked} kali</div>}
+            </div>
+          </header>
+          <About/>
+        </div>
+        <div className="version">- Version 1.0.12</div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/component/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="social-media">
+      Created by <b>MirukuSheki</b> <br/>
+      <b>Github:</b> <a href="https://github.com/mirukusheki" target="_blank">mirukusheki</a><br/>
+      <b>Paypal: </b><a href="https://paypal.me/RJKuker?locale.x=id_ID" target="_blank">paypal.me/RJKuker</a><br/>
+      <small>(You can Donate to me for more development or just enjoying my web page)</small><br/>
+      <b>Repositories:</b> <a href="https://github.com/mirukusheki/ashiyaap"
+                              target="_blank">https://github.com/mirukusheki/ashiyaap</a>
+      <br/><br/>
+      Found any bug? <a href="https://github.com/mirukusheki/ashiyaap/issues" target="_blank">Let me know</a>
+    </div>
+  );
+};
+
+export default About;
